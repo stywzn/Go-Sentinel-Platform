5439ccd5d16f72d376a325ab0f22e87b69ede827
stywzn <jhonynnis@gmail.com>
2026-02-10 11:22:49 +0800

    update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,27 +4,42 @@ import (
 	"log"
 	"net"
 
+	"google.golang.org/grpc"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
 	pb "github.com/stywzn/Go-Cloud-Compute/api/proto"
 	"github.com/stywzn/Go-Cloud-Compute/internal/server"
-	"google.golang.org/grpc"
 )
 
 func main() {
-	port := ":9090"
-	lis, err := net.Listen("tcp", port)
+
+	dsn := "root:root@tcp(127.0.0.1:3306)/cloud_compute?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatalf(" 端口被占用或无法监听: %v", err)
+		log.Fatalf(" 无法连接数据库: %v", err)
 	}
+	log.Println(" 数据库连接成功!")
 
-	grpcServer := grpc.NewServer()
-
-	handler := &server.SentinelServer{}
-	pb.RegisterSentinelServiceServer(grpcServer, handler)
-
-	log.Printf(" Sentinel Control Plane 已启动 | 监听端口 %s", port)
-
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf(" 服务器崩溃: %v", err)
+	err = db.AutoMigrate(&server.AgentModel{})
+	if err != nil {
+		log.Fatalf(" 自动建表失败: %v", err)
 	}
+	log.Println(" 数据库表结构同步完成")
 
+	lis, err := net.Listen("tcp", ":9090")
+	if err != nil {
+		log.Fatalf("端口监听失败: %v", err)
+	}
+	log.Println("Sentinel Control Plane 已启动 | 监听端口 :9090")
+
+	s := grpc.NewServer()
+
+	srv := &server.SentinelServer{DB: db}
+	pb.RegisterSentinelServiceServer(s, srv)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("gRPC 服务启动失败: %v", err)
+	}
 }
--- internal/server/handler.go
+++ internal/server/handler.go
@@ -2,22 +2,49 @@ package server
 
 import (
 	"context"
-	"io"
 	"log"
 	"time"
 
-	"github.com/google/uuid"
 	pb "github.com/stywzn/Go-Cloud-Compute/api/proto"
+	"gorm.io/gorm"
 )
+
+type AgentModel struct {
+	gorm.Model
+	AgentID  string `gorm:"uniqueIndex;size:191"`
+	Hostname string
+	IP       string
+	Status   string
+}
 
 type SentinelServer struct {
 	pb.UnimplementedSentinelServiceServer
+	DB *gorm.DB
 }
 
 func (s *SentinelServer) Register(ctx context.Context, req *pb.RegisterReq) (*pb.RegisterResp, error) {
-	agentID := uuid.New().String()
+	agentID := req.Hostname
 
-	log.Printf("[Register] 新节点加入 Host: %s, IP: %s, ID: %s", req.Hostname, req.Ip, agentID)
+	log.Printf(" [Register] 收到注册请求: %s (%s)", req.Hostname, req.Ip)
+
+	var agent AgentModel
+	result := s.DB.Where("agent_id = ?", agentID).First(&agent)
+
+	if result.Error != nil {
+		newAgent := AgentModel{
+			AgentID:  agentID,
+			Hostname: req.Hostname,
+			IP:       req.Ip,
+			Status:   "online",
+		}
+		s.DB.Create(&newAgent)
+		log.Println(" [DB] 新节点已入库")
+	} else {
+		agent.Status = "Online"
+		agent.IP = req.Ip
+		s.DB.Save(&agent)
+		log.Println(" [DB] 节点信息已更新")
+	}
 
 	return &pb.RegisterResp{
 		AgentId: agentID,
@@ -27,37 +54,30 @@ func (s *SentinelServer) Register(ctx context.Context, req *pb.RegisterReq) (*pb
 
 func (s *SentinelServer) Heartbeat(stream pb.SentinelService_HeartbeatServer) error {
 	for {
+
 		req, err := stream.Recv()
-		if err == io.EOF {
-			log.Println(" Agent 断开连接")
-			return nil
-		}
+
 		if err != nil {
 			log.Printf(" 接收错误: %v", err)
 			return err
 		}
 
-		log.Printf(" [Heartbeat] ID: %s", req.AgentId)
+		log.Printf(" [Heartbeat] 来自: %s", req.AgentId)
 
-		var jobToSend *pb.Job = nil
-
-		if time.Now().Unix()%5 == 0 {
+		if time.Now().Unix()%10 == 0 {
 			log.Printf(" [Dispatch] 正在派发 PING 任务给 %s", req.AgentId)
-
-			jobToSend = &pb.Job{
-				JobId:   uuid.New().String(),
-				Type:    pb.JobType_PING,
-				Payload: "8.8.8.8",
+			err := stream.Send(&pb.HeartbeatResp{
+				Job: &pb.Job{
+					JobId:   "job-" + req.AgentId,
+					Type:    pb.JobType_PING,
+					Payload: "8.8.8.8",
+				},
+			})
+			if err != nil {
+				return err
 			}
-		}
-
-		err = stream.Send(&pb.HeartbeatResp{
-			ConfigOutdated: false,
-			Job:            jobToSend,
-		})
-		if err != nil {
-			log.Printf(" 发送失败: %v", err)
-			return err
+		} else {
+			stream.Send(&pb.HeartbeatResp{ConfigOutdated: false})
 		}
 	}
 }
@@ -66,8 +86,5 @@ func (s *SentinelServer) ReportJobStatus(ctx context.Context, req *pb.ReportJobR
 
 	log.Printf(" [Report] 收到任务汇报! Agent: %s | Job: %s | 状态: %s | 结果: %s",
 		req.AgentId, req.JobId, req.Status, req.Result)
-
-	return &pb.ReportJobResp{
-		Received: true,
-	}, nil
+	return &pb.ReportJobResp{Received: true}, nil
 }
